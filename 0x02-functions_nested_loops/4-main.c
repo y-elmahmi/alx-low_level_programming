ad37fef1dbe8060ab7b44a266cef1eca35c87b4a
y-elmahmi <youssef.elmahmi@gmail.com>
2023-03-21 20:09:16 -0700

    4-isalpha scripts and 4-main.c programme done !

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- *main - check the code.
+ * main - check the code.
  *
- *Return: Always 0.
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int r;
+    int r;
 
 	r = _isalpha('H');
 	_putchar(r + '0');
