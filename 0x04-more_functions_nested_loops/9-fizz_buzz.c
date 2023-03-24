8d8db78d491598a26ca8fea249944eb50adac9cb
y-elmahmi <youssef.elmahmi@gmail.com>
2023-03-23 19:57:33 -0700

    function 9-fizz_buzz.c Done <>

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,19 +18,20 @@ int main(void)
 		{
 			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
-		{												
+		{
 			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)					
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		} else if (i == 1)
-		{				
+		{
 			printf("%d", i);
 		} else
 		{
 			printf(" %d", i);
 		}
 	}
-		printf("\n");
-			return (0);
+	printf("\n");
+
+	return (0);
 }
