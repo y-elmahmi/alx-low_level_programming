4c542987a9673a1183e6e31012b528e735943a35
y-elmahmi <youssef.elmahmi@gmail.com>
2023-04-08 16:37:37 -0700

    Problem fixed

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 gcc -Wall -Werror -Wextra -pedantic *.c
+
+# Create the static library liball.a from the object files
 ar -rcs liball.a *.o
+
+# Index the library for faster symbol lookup
 ranlib liball.a
 
+# Clean up the .o files
+rm -f *.o
