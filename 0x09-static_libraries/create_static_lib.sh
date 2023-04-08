acd9129606bd893b25202af602bc916413d1f877
y-elmahmi <youssef.elmahmi@gmail.com>
2023-04-08 16:45:38 -0700

    Update script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-gcc -Wall -Werror -Wextra -pedantic *.c
+gcc -Wall -Werror -Wextra -pedantic -c *.c
 
 # Create the static library liball.a from the object files
-ar -rcs liball.a *.o
+ar -rc liball.a *.o
 
 # Index the library for faster symbol lookup
 ranlib liball.a
 
-# Clean up the .o files
-rm -f *.o
